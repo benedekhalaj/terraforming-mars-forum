9f1ff56308be51b0dba3afc7c3bed54cf4fd5f32
sagi-viktor <sagi.viktor1@gmail.com>
2022-08-11 13:18:04 +0200

    chore: add files and connections related to comments

--- src/main/java/com/codecool/terraformingmarsforum/model/UserPost.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.terraformingmarsforum.model;
-
-import com.codecool.terraformingmarsforum.model.types.PostType;
-
-public class UserPost extends Post {
-    public UserPost(int id, String title, String description, PostType type, String author) {
-        super(id, title, description, type, author);
-    }
-}
