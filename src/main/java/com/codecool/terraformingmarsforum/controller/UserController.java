48adf507ca2d79e3093cd8d74ff6af645076a867
sagi-viktor <sagi.viktor1@gmail.com>
2022-08-11 19:29:32 +0200

    chore: add api route to create user

--- src/main/java/com/codecool/terraformingmarsforum/controller/UserController.java
+++ src/main/java/com/codecool/terraformingmarsforum/controller/UserController.java
@@ -1,11 +1,30 @@
 package com.codecool.terraformingmarsforum.controller;
 
 
+import com.codecool.terraformingmarsforum.model.User;
+import com.codecool.terraformingmarsforum.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
+
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
+    @PostMapping("/")
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        int id = userService.createUser(user);
+        return ResponseEntity.created(URI.create(String.format("api/comments/%d", id))).body(user);
+    }
+
 }
+
--- src/main/java/com/codecool/terraformingmarsforum/service/UserService.java
+++ src/main/java/com/codecool/terraformingmarsforum/service/UserService.java
@@ -1,6 +1,7 @@
 package com.codecool.terraformingmarsforum.service;
 
 
+import com.codecool.terraformingmarsforum.model.User;
 import com.codecool.terraformingmarsforum.service.DAO.UserDAO;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -12,4 +13,8 @@ public class UserService {
 
     @NonNull
     private UserDAO userDAO;
+
+    public int createUser(User user) {
+        return userDAO.createUser(user).getId();
+    }
 }
