bff008920dbfc75b099ad66b0125438d0a031a5c
sagi-viktor <sagi.viktor1@gmail.com>
2022-08-11 10:14:36 +0200

    chore: add comment related files

--- src/main/java/com/codecool/terraformingmarsforum/controller/CommentController.java
+++ src/main/java/com/codecool/terraformingmarsforum/controller/CommentController.java
@@ -1,9 +1,4 @@
 package com.codecool.terraformingmarsforum.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/comment")
 public class CommentController {
 }
--- src/main/java/com/codecool/terraformingmarsforum/service/CommentService.java
+++ src/main/java/com/codecool/terraformingmarsforum/service/CommentService.java
@@ -1,13 +1,4 @@
 package com.codecool.terraformingmarsforum.service;
 
-import com.codecool.terraformingmarsforum.service.DAO.CommentDAO;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
 public class CommentService {
-
-    private CommentDAO commentDAO;
-
 }
