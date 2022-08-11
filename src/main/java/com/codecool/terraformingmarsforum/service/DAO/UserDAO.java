3aa6a47b440c478fbc8e8d430cbc5a717c0b4f52
sagi-viktor <sagi.viktor1@gmail.com>
2022-08-11 19:29:13 +0200

    chore: add user memory

--- src/main/java/com/codecool/terraformingmarsforum/service/DAO/UserDAO.java
+++ src/main/java/com/codecool/terraformingmarsforum/service/DAO/UserDAO.java
@@ -1,4 +1,7 @@
 package com.codecool.terraformingmarsforum.service.DAO;
 
+import com.codecool.terraformingmarsforum.model.User;
+
 public interface UserDAO {
+    User createUser(User user);
 }
--- src/main/java/com/codecool/terraformingmarsforum/service/DAO/memory/UserMemory.java
+++ src/main/java/com/codecool/terraformingmarsforum/service/DAO/memory/UserMemory.java
@@ -1,8 +1,23 @@
 package com.codecool.terraformingmarsforum.service.DAO.memory;
 
+import com.codecool.terraformingmarsforum.model.User;
 import com.codecool.terraformingmarsforum.service.DAO.UserDAO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
+@RequiredArgsConstructor
 public class UserMemory implements UserDAO {
+
+    private final Set<User> users;
+    private int idCounter;
+
+    @Override
+    public User createUser(User user) {
+        users.add(user);
+        user.setId(idCounter++);
+        return user;
+    }
 }
