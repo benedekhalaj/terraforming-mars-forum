7e06a5318de32c4232ff2fc157e13d9c86f289b1
sagi-viktor <sagi.viktor1@gmail.com>
2022-08-11 10:14:36 +0200

    chore: add comment related files

--- src/main/java/com/codecool/terraformingmarsforum/model/Comment.java
+++ src/main/java/com/codecool/terraformingmarsforum/model/Comment.java
@@ -1,10 +1,4 @@
 package com.codecool.terraformingmarsforum.model;
 
-import com.codecool.terraformingmarsforum.service.DAO.CommentDAO;
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class Comment {
-
-    @Autowired
-    private CommentDAO commentDAO;
 }
--- src/main/java/com/codecool/terraformingmarsforum/service/DAO/memory/CommentMemory.java
+++ src/main/java/com/codecool/terraformingmarsforum/service/DAO/memory/CommentMemory.java
@@ -1,11 +1,4 @@
 package com.codecool.terraformingmarsforum.service.DAO.memory;
 
-import com.codecool.terraformingmarsforum.service.DAO.CommentDAO;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class CommentMemory implements CommentDAO {
-
-
-
+public class CommentMemory {
 }
