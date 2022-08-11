2b818686c1326cc446f495e04b515cc2e5dc252f
sagi-viktor <sagi.viktor1@gmail.com>
2022-08-11 10:14:36 +0200

    chore: add comment related files

---
+++ src/main/java/com/codecool/terraformingmarsforum/controller/CommentController.java
@@ -0,0 +1,4 @@
+package com.codecool.terraformingmarsforum.controller;
+
+public class CommentController {
+}
---
+++ src/main/java/com/codecool/terraformingmarsforum/service/DAO/memory/CommentMemory.java
@@ -0,0 +1,4 @@
+package com.codecool.terraformingmarsforum.service.DAO.memory;
+
+public class CommentMemory {
+}
